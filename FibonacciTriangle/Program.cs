9829cd947b807e0453c4864693a0667ab810e617
Md. Shihab <shihabrayhan8@gmail.com>
2021-09-22 16:56:43 +0600

    Program has been done....

--- FibonacciTriangle/Program.cs
+++ FibonacciTriangle/Program.cs
@@ -12,6 +12,26 @@ namespace FibonacciTriangle
             //print show message
             Console.Write("Please Enter the limit: ");
 
+            //get user input
+            n = int.Parse(Console.ReadLine());
+
+            //create a for loop to continue program
+            for (i = 1; i <= n; i++)
+            {
+                a = 0;
+                b = 1;
+                Console.Write(b + "\t");
+
+                for(j = 1; j < i; j++)
+                {
+                    c = a + b;
+                    Console.Write(c + "\t");
+
+                    a = b;
+                    b = c;
+                }
+                Console.Write("\n");
+            }
         }
     }
 }
